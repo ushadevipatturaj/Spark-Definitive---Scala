c7859010eabdda9a60b57fd254902bb8eaef7cfa
ushapatturaj <ushadevipatturaj@gmail.com>
2020-05-08 16:40:59 +0200

    Definitive Spark - groupby and count using sql way and spark way

--- src/main/scala/com/SparkDefinitiveScala/Chapter/one/Dataframes_Tutorial_1.scala
+++ src/main/scala/com/SparkDefinitiveScala/Chapter/one/Dataframes_Tutorial_1.scala
@@ -9,7 +9,17 @@ object Dataframes_Tutorial_1 extends App with Context {
     .option("inferSchema",value = true)
     .csv("D:\\Study_Materials\\spark-definitive-scala\\src\\main\\resources\\2015-summary.csv")
   val take5 = dfFlightData.take(5)
-  println(s"Result od Dataframe.take(5) ${take5.mkString(",")}")
+  println(s"Result of Dataframe.take(5) ${take5.mkString(",")}")
   dfFlightData.show(10)
   dfFlightData.sort("count").explain()
+
+  //in SQL way
+  dfFlightData.createOrReplaceTempView("flight_view")
+  val sqlWay = spark.sql("select DEST_COUNTRY_NAME,count(DEST_COUNTRY_NAME) from flight_view group by DEST_COUNTRY_NAME")
+  val sparkWay = dfFlightData.groupBy("DEST_COUNTRY_NAME").count()
+  sqlWay.show()
+  sparkWay.show()
+
+  sqlWay.explain()
+  sparkWay.explain()
 }
