829b7c3ad07581d067ec96a2eabf8e9efa1f87d8
ushapatturaj <ushadevipatturaj@gmail.com>
2020-05-11 16:33:53 +0200

    Definitive Spark - Reading and writing streams of dataframes in spark

--- src/main/scala/com/SparkDefinitiveScala/Chapter/one/Dataframes_Tutorial_3.scala
+++ src/main/scala/com/SparkDefinitiveScala/Chapter/one/Dataframes_Tutorial_3.scala
@@ -10,9 +10,33 @@ object Dataframes_Tutorial_3 extends App with Context {
   val staticSchema = dfRetail.schema
   println(staticSchema.fields.mkString(","))
   //getting details of which customer makes most of the shopping at which time
+
+  import spark.implicits._
   dfRetail.selectExpr("CustomerID","(Quantity * UnitPrice) as total_cost","InvoiceDate")
-    .groupBy(col("CustomerID"),window(col("InvoiceDate"),"1 day"))
+    .groupBy($"CustomerID",window($"InvoiceDate","1 day"))
     .sum("total_cost").orderBy(desc("sum(total_cost)")).show(5,truncate = false)
+
+  //streaming of files - read stream
+  val dfStream = spark.readStream
+    .option("header",value = true)
+    .option("maxFilesPerTrigger" ,1)
+    .format("csv")
+    .schema(staticSchema)
+    .load("D:\\Study_Materials\\spark-definitive-scala\\src\\main\\resources\\2010*.csv")
+
+  val customerPurchase = dfStream.selectExpr("CustomerID","(Quantity * UnitPrice) as total_cost ","InvoiceDate")
+    .groupBy($"CustomerID",window($"InvoiceDate" ,"1 day"))
+    .sum("total_cost").orderBy(desc("sum(total_cost)"))
+
+  println(s"Is streaming in progress? ${dfStream.isStreaming}")
+
+  //streaming of files - write stream
+  val topPurchaseCustomer = customerPurchase.writeStream
+    .format("console")
+    .queryName("TopPurchases")
+    .outputMode("complete")
+    .start()
+
 
 
 }
