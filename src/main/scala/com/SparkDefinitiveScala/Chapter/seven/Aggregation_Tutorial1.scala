43ad746156ac9be2e89472abacb22aa703ca5ce6
ushapatturaj <ushadevipatturaj@gmail.com>
2020-05-20 19:51:49 +0200

    Definitive Spark -Aggregations - count,countDistinct,approximate_count_distinct(),min(),max(),first(),last(),sum(),getting first row of a dataframe using first()

---
+++ src/main/scala/com/SparkDefinitiveScala/Chapter/seven/Aggregation_Tutorial1.scala
@@ -0,0 +1,19 @@
+package com.SparkDefinitiveScala.Chapter.seven
+import com.SparkDefinitiveScala.Chapter.one.Context
+import org.apache.spark.sql.functions._
+object Aggregation_Tutorial1 extends App with Context {
+  val dfCSV = spark.read
+    .option("header",value = true)
+    .option("inferSchema", value = true)
+    .csv("D:\\Study_Materials\\spark-definitive-scala\\src\\main\\resources\\2010-12-01.csv")
+  import spark.implicits._
+  //count,countDistinct,approximate_Count
+  dfCSV.select(count("StockCode"),countDistinct("StockCode")
+    ,approx_count_distinct("StockCode",0.1)).show(10,truncate = true)
+
+  //sum,first,last,min,max
+  dfCSV.select(sum("Quantity"),min("Quantity"),max("Quantity")
+  ,first("Description"),last("Description")).show(10,truncate = true)
+  val firstRow = dfCSV.first()
+  println(firstRow.mkString(","))
+}
