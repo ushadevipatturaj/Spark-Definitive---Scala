5c20bc2759e663589e638eb99f896f4137fa2752
ushapatturaj <ushadevipatturaj@gmail.com>
2020-05-20 11:47:12 +0200

    Definitive Spark -Date/Timestamp Functions - Creating complex datatypes using Struct, creating manual complex schema using Struct, Creating manual data, Creating normal file from a complex datatype data

--- src/main/scala/com/SparkDefinitiveScala/Chapter/six/SparkTypes_Tutorial3.scala
+++ src/main/scala/com/SparkDefinitiveScala/Chapter/six/SparkTypes_Tutorial3.scala
@@ -1,6 +1,8 @@
 package com.SparkDefinitiveScala.Chapter.six
 import com.SparkDefinitiveScala.Chapter.one.Context
+import org.apache.spark.sql.Row
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
 object SparkTypes_Tutorial3 extends App with Context{
   val dfCSV = spark.read
     .option("header",value = true)
@@ -9,4 +11,35 @@ object SparkTypes_Tutorial3 extends App with Context{
   import spark.implicits._
   dfCSV.orderBy($"Description".asc_nulls_first).show(10,truncate = false)
   dfCSV.orderBy($"Description".desc_nulls_last).show(10,truncate = false)
+
+  //complex types of spark
+  val dfComplex = dfCSV.select(struct("InvoiceNo","Description").as("Complex"))
+  dfComplex.createOrReplaceTempView("tempcomplexview")
+  dfComplex.printSchema()
+  dfComplex.select($"Complex".getField("InvoiceNo"),$"Complex".getField("Description"),
+  $"Complex.*").show(3, truncate = false)
+
+  //creating manual schema using struct and manual data
+  val manualComplexSchema = new StructType().
+    add("Name",new StructType()
+      .add("FirstName",StringType)
+      .add("LastName",StringType))
+    .add("Class",StringType)
+    .add("RollNo",IntegerType)
+
+  val manualComplexData = Seq(
+    Row(Row("Usha Devi","Patturaj"),"Seventh Std",37),
+    Row(Row("Sai Prasad","Gorre"),"Fifth Std",15)
+  )
+
+  val rddSchool = spark.sparkContext.parallelize(manualComplexData)
+  val dfSchool = spark.createDataFrame(rddSchool,manualComplexSchema)
+  dfSchool.printSchema()
+  val dfNormalSchool =dfSchool.withColumn("FirstName",$"Name".getField("FirstName"))
+    .withColumn("LastName",$"Name".getField("LastName"))
+    .withColumn("Class",$"Class")
+    .withColumn("RollNumber",$"RollNo")
+    .select("FirstName","LastName","Class","RollNumber")
+  dfNormalSchool.show(truncate =false)
+
 }
