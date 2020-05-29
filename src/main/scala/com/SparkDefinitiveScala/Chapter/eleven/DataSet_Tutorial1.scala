d70e1ad20036ee7d19cfbfb179bfb3b4ab5c8784
ushapatturaj <ushadevipatturaj@gmail.com>
2020-05-29 15:29:29 +0200

    Definitive Spark -Dataset - Filter(),Map(),and joining 2 datasets

--- src/main/scala/com/SparkDefinitiveScala/Chapter/eleven/DataSet_Tutorial1.scala
+++ src/main/scala/com/SparkDefinitiveScala/Chapter/eleven/DataSet_Tutorial1.scala
@@ -10,4 +10,25 @@ object DataSet_Tutorial1 extends App with Context{
   case class flight_data(DEST_COUNTRY_NAME : String ,ORIGIN_COUNTRY_NAME : String,count : BigInt)
   val flightsDataset:Dataset[flight_data] = parquetFile.as[flight_data]
   flightsDataset.take(10).foreach(row => println(row .DEST_COUNTRY_NAME,row.ORIGIN_COUNTRY_NAME,row.count))
+  //filtering the dataset using a function
+  def checkDomestic(flightRow :flight_data):Boolean={
+    flightRow.ORIGIN_COUNTRY_NAME == flightRow.DEST_COUNTRY_NAME
+  }
+  def checkIndia(flightRow :flight_data):Boolean={
+    (flightRow.ORIGIN_COUNTRY_NAME == "India") | ( flightRow.DEST_COUNTRY_NAME == "India")
+  }
+
+  flightsDataset.filter(row => checkDomestic(row)).show(10)
+  flightsDataset.filter(row =>checkIndia(row)).show(10)
+
+  //Mapping
+  flightsDataset.take(20).map(flight => "The New "+flight.DEST_COUNTRY_NAME)
+
+  //another case class to join with flight
+  case class randomFlight(count:BigInt,randomvalue:BigInt)
+  val randomFlightDataset = spark.range(100).map(row => (row,scala.util.Random.nextLong)).withColumnRenamed("_1","count")
+    .withColumnRenamed("_2","randomvalue").as[randomFlight]
+  randomFlightDataset.show(10)
+
+  randomFlightDataset.join(flightsDataset,Seq("count")).show(10)
 }
