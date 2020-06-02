2281b1d1809f9bb9c36e968af5e7342e754a1946
ushapatturaj <ushadevipatturaj@gmail.com>
2020-06-02 20:16:55 +0200

    Definitive Spark -Low level RDD - reduce,count,countApprox,countApproxDistinct,countByValue,CountByValueApprox

--- src/main/scala/com/SparkDefinitiveScala/Chapter/twelve/LowlevelRDD_Tutorial1.scala
+++ src/main/scala/com/SparkDefinitiveScala/Chapter/twelve/LowlevelRDD_Tutorial1.scala
@@ -19,4 +19,17 @@ object LowlevelRDD_Tutorial1 extends App with Context{
   val split_val = rdd_spark.randomSplit(Array[Double](0.5,0.5)).toSeq
   println(split_val)
 
+  //reduce
+  val sum = spark.sparkContext.parallelize(1 to 20).reduce(_ + _)
+  println(s"sum is $sum")
+
+  //count and count approx
+  val count_actual = rdd_spark.count()
+  val count_approx = rdd_spark.countApprox(500,0.95)
+  println(s"count $count_actual and count_approximate $count_approx")
+
+  val count_distinct = rdd_spark.countApproxDistinct()
+  val count_value = rdd_spark.countByValue()
+  val count_value_approx = rdd_spark.countByValueApprox(500,0.95)
+  println(s"count approximate distinct $count_distinct , countbyvalue $count_value and countby value approximate $count_value_approx")
 }
